cc46eb4f5ad8f961b3f028777d9f2ab41d950d75
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-04-19 14:50:50 +0700

    Add arquero

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useData } from "./hooks/useData";
+import * as aq from "arquero";
 function App() {
   const { data, isLoading, error } = useData();
   // useEffect(() => {
@@ -10,6 +11,20 @@ function App() {
   //     })
   //     .getAllSheetsData();
   // }, []);
+  useEffect(() => {
+    const dt = aq.table({
+      city: ["Seattle", "Chicago", "San Francisco"],
+      sun_hours: [157, 120, 205],
+    });
+
+    // 2. Transform the data: Filter and Derive
+    const result = dt
+      .derive({ sun_hours: (d) => d.sun_hours + 10 }) // Add 10 to sun hours
+      .filter((d) => d.sun_hours > 150); // Keep only if > 150
+
+    // 3. Print the resulting table
+    result.print();
+  }, []);
   return (
     <>
       <h1>My Dashboard</h1>
